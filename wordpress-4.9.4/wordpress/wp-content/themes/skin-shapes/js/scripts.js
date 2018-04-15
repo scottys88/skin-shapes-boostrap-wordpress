5c096da451594f5e80b8583db9729058db430650
scottys88 <scott_schubert@hotmail.com.au>
2018-04-15 17:04:03 +0930

    Added sticky menu with scroll funciton

--- wordpress-4.9.4/wordpress/wp-content/themes/skin-shapes/js/scripts.js
+++ wordpress-4.9.4/wordpress/wp-content/themes/skin-shapes/js/scripts.js
@@ -94,9 +94,27 @@ window.setTimeout(function(){
 */
 
 
+var lastScrollTop = 0;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+	var menu = document.getElementById("wrapper-navbar");
+	if(document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+		   if (st > lastScrollTop){
+			   menu.classList.remove('Scroll-up');
+			   menu.classList.add('Scroll-down');
+		   } else {
+			  menu.classList.remove('Scroll-down');
+			  menu.classList.add('Scroll-up');
+		   }
+		   lastScrollTop = st;
+	}
+
+} , false);
 
 
     
+
 
 
 	
